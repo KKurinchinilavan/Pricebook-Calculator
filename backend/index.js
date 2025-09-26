af8409c53f7c0f259836b925dd053acf537f73b9
KKurinchinilavan <kurinchi09032@gmail.com>
2025-09-26 22:04:59 +0530

    Initial commit: add backend and frontend folders

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+
+// Load your JSON file
+const data = JSON.parse(fs.readFileSync("./data/globalpricebook.json"));
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+
+// 1️⃣ Get all countries for dropdown
+app.get("/countries", function (req, res) {
+  const countries = [...new Set(data.map(function (item) {
+    return item.Country;
+  }))];
+  res.json(countries);
+});
+
+// Endpoint to get data for a specific country
+app.get("/country/:name", function (req, res) {
+  const countryName = req.params.name.toLowerCase(); // convert to lowercase
+
+  // Filter the JSON data for the selected country (case-insensitive)
+  const countryData = data.filter(item => item.Country.toLowerCase() === countryName);
+
+  if (countryData.length === 0) {
+    return res.status(404).json({ error: "Country not found" });
+  }
+
+  res.json(countryData);
+});
+
+
+
+
+
+const PORT = 5000;
+app.listen(PORT, function () {
+  console.log("Server running on http://localhost:" + PORT);
+});
